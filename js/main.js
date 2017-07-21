24b010e822655f41ed25bee415b313036d3a7752
Matheus Barone <matheus.barone@kbrtec.com.br>
2017-07-21 17:03:51 -0300

    ajustes na kbr

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 // CRIAR A LISTA
-
-function addTodoListItem() {
+function addTodoListEvent() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("input").value;
+    var lista = document.getElementById("lista");
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
+
     if (!inputValue) {
         alert("O campo precisa ser preenchido");
     } else {
-        document.getElementById("lista").appendChild(li);
+        lista.appendChild(li);
     }
     document.getElementById("input").value = "";
-}
 
-document.getElementById("buttonAdd").addEventListener("click", addTodoListItem);
+
+    // CLicar no X para fechar o item
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+};
+
+document.getElementById("buttonAdd").addEventListener("click", addTodoListEvent);
