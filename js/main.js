4362cb7dbcae12edce0fa7ca487bdf309f5bbfc6
Matheus Barone <matheusvbarone@gmail.com>
2017-07-22 17:33:49 -0300

    tentando habilitar a opção de apagar os itens ao apertar esc

--- js/main.js
+++ js/main.js
@@ -1,38 +1,40 @@
-var btn = document.getElementById("buttonAdd");
+// CRIANDO A FUNCÃO QUE ADICIONA UM ITEM NOVO
 
-// CRIAR ELEMENTOS NOVOS NA LISTA
-function createNewElement() {
+
+
+
+
+var btn = document.getElementById("buttonAdd")
+
+function addNewElement() {
+
+    // CRIANDO A FUNCAO QUE VAI ADICIONAR O ELEMENTO
+    var ul = document.getElementById("lista");
     var li = document.createElement("li");
-    var inputValue = document.getElementById("input").value;
+    li.className = "element";
     var input = document.getElementById("input");
+    var inputValue = input.value;
     var p = document.createElement("p");
-    var t = document.createTextNode(inputValue);
+    var text = document.createTextNode(inputValue);
+    p.appendChild(text);
     p.className = "textoItem";
-    p.appendChild(t);
     li.appendChild(p);
 
-
     if (!inputValue) {
-        alert("O campo precisa ser preenchido");
-    } else if (input.value.length > 50) {
-        alert("O numero máximo de caracteres é 50, voce digitou " + input.value.length + " caracteres.");
+        alert("Você precisa preecher o campo");
     } else {
-        lista.appendChild(li);
+        ul.appendChild(li);
     }
-    document.getElementById("input").value = "";
+    input.value = "";
 
-
-
-
-    // CRIANDO UM BOTÃO PARA FECHAR O ELEMENTO CRIADO
-
+    // CRIANDO A FUNCAO QUE VAI ADICIONAR O BOTÃO DENTRO DO ELEMENTO
     var span = document.createElement("span");
     var txt = document.createTextNode("\u00D7");
     var close = document.getElementsByClassName("close");
-
-    span.className = "close";
-    span.appendChild(txt);
     li.appendChild(span);
+    span.appendChild(txt);
+    span.className = ("close");
+    var i;
 
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
@@ -40,14 +42,128 @@ function createNewElement() {
             div.style.display = "none";
         }
     }
-}
+};
 
 
 
-btn.addEventListener("click", createNewElement);
-
-document.addEventListener('keyup', function(e) {
+// CRIAR ELEMENTO QUANDO APERTAR O ENTER
+btn.addEventListener("click", addNewElement);
+document.addEventListener("keyup", function(e) {
     if (e.keyCode == 13) {
-        createNewElement();
+        addNewElement();
     }
 });
+
+// APAGAR ELEMENTO QUANDO APERTAR O ESC
+btn.addEventListener("click", addNewElement);
+document.addEventListener("keyup", function(e) {
+    var ul = document.getElementsByClassName("lista");
+    var li = document.getElementsByClassName("element");
+    for (i = 0; i < ul.length; i++) {
+        if (e.keyCode == 27) {
+            console.log(ul[i].lastChild);
+            ul[i].lastChild.style.display = "none";
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var btn = document.getElementById("buttonAdd");
+
+// // CRIAR ELEMENTOS NOVOS NA LISTA
+// function createNewElement() {
+//     var li = document.createElement("li");
+//     var inputValue = document.getElementById("input").value;
+//     var input = document.getElementById("input");
+//     var p = document.createElement("p");
+//     var t = document.createTextNode(inputValue);
+//     p.className = "textoItem";
+//     p.appendChild(t);
+//     li.appendChild(p);
+
+
+//     if (!inputValue) {
+//         alert("O campo precisa ser preenchido");
+//     } else if (input.value.length > 50) {
+//         alert("O numero máximo de caracteres é 50, voce digitou " + input.value.length + " caracteres.");
+//     } else {
+//         lista.appendChild(li);
+//     }
+//     document.getElementById("input").value = "";
+
+
+
+
+//     // CRIANDO UM BOTÃO PARA FECHAR O ELEMENTO CRIADO
+
+//     var span = document.createElement("span");
+//     var txt = document.createTextNode("\u00D7");
+//     var close = document.getElementsByClassName("close");
+
+//     span.className = "close";
+//     span.appendChild(txt);
+//     li.appendChild(span);
+
+//     for (i = 0; i < close.length; i++) {
+//         close[i].onclick = function() {
+//             var div = this.parentElement;
+//             div.style.display = "none";
+//         }
+//     }
+// }
+
+
+
+// btn.addEventListener("click", createNewElement);
+
+// document.addEventListener('keyup', function(e) {
+//     if (e.keyCode == 13) {
+//         createNewElement();
+//     }
+// });
